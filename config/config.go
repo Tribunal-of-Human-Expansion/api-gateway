1e11f8fae38401cb3f9a19254e32fdfc6c129a00
batunii <sy.soni@outlook.com>
2026-03-13 13:07:19 +0000

    Added Auth and Config Modules with Good comments for Auth Module

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port            string
+	JWTSecret       string
+	RateLimitMax    int
+	RateLimitWindow int // seconds
+
+	BookingURL          string
+	CompatibiltyService string
+	UserService         string // All the other sevices wil come here first
+}
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Encountered error in loading the Dot env, please check if its there.")
+	}
+	return &Config{
+		Port:                getEnv("PORT", "8080"),
+		JWTSecret:           getEnv("JWT_SECERET", ""),
+		RateLimitMax:        getEnvInt("RATE_LIMIT_MAX", 100),
+		RateLimitWindow:     getEnvInt("RATE_LIMIT_WINDOW", 60),
+		BookingURL:          getEnv("BOOKIN_URL", ""),
+		CompatibiltyService: getEnv("COMPT_SERV_URL", ""),
+		UserService:         getEnv("USER_SERVICE", ""),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
+func getEnvInt(key string, fallback int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	var result int
+	_, err := fmt.Sscanf(val, "%d", &result)
+	if err != nil {
+		fmt.Printf("Encountered an error : %v \n", err)
+		return fallback
+	}
+	return result
+}
